20f9e0867c264fb505b2dd1d2be2637a06d7dad5
Marco Barbero Mota <marco.barbero.mota@vanderbilt.edu>
2023-05-22 13:38:37 -0500

    added to load_pickle: folder_path and file_name

--- prefect_utils/utils.py
+++ prefect_utils/utils.py
@@ -1,9 +1,11 @@
 import pickle 
+import os
 import base64
 from pathlib import Path
 from prefect.results import PersistedResultBlob
 
-def load_pickle(path_to_file):
+def load_pickle(path_to_folder, file_name):
+    path_to_file = os.path.join(path_to_folder,file_name)
     object = pickle.loads(
                 base64.b64decode(
                 PersistedResultBlob.parse_raw(
