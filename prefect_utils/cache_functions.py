390139a15b301924b4e2d7416a0a18a2e59443c1
Marco Barbero Mota <marco.barbero.mota@vanderbilt.edu>
2023-06-06 22:57:17 -0500

    final file based and content based cache functions

--- prefect_utils/cache_functions.py
+++ prefect_utils/cache_functions.py
@@ -8,7 +8,6 @@ from prefect.results import PersistedResultBlob
 def cache_constant(task_context, task_parameters):
     return 'dummy'
 
-
 def cache_file_based_fn(task_context, task_parameters):
     """
     Local file based cache function to be used in prefect @task and @ flow decorators in the cache_key_fn attribute.
@@ -27,15 +26,35 @@ Returns:
     """
 
     # Check if the result file exists
+    try:
+        if os.path.exists(task_parameters["result_file_path"]):
+            return "exists"
+    except:
+        return None
+
+def cache_file_content_based_fn(task_context, task_parameters):
+    """
+    Local file and content based cache function to be used in prefect @task and @ flow decorators in the cache_key_fn attribute.
+
+    IMPORTANT: It requires the @task or @flow to include a result_file_path attribute. This is the path (including filename
+    and extension of the resulting file where the @task or @flow would be saving the result python object.
+
+    The function checks whether the result file exists and contains the same content as in the previous run. 
+    If both conditions are met the @task or @flow will not run but 
+    grab the result object from the file and return it instead.
+
+Args:
+    task_context, task_parameters (requires The @task or @flow to include a result_file_path attribute)
+
+Returns:
+    str:file hash
+    """
+
+    # Check if the result file exists
     if os.path.exists(task_parameters["result_file_path"]):
-        # Compute the hash of the result file
-        hash_md5 = hashlib.md5()
-        with open(task_parameters["result_file_path"], 'rb') as f:
-            for chunk in iter(lambda: f.read(4096), b""):
-                hash_md5.update(chunk)
-            file_hash = hash_md5.hexdigest()
-        # Return the hash as the cache key
-        return file_hash
+        cache_key = hashlib.md5(
+            PersistedResultBlob.parse_raw(Path(task_parameters['result_file_path']).read_bytes()).data).hexdigest()
+        return cache_key
     else:
         return None
 
